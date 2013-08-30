3e15cc14178b2e1d5fb3ebc8b5f0169018fb744a
Steve Polyak <stevepolyak@mac.com>
2013-08-30 15:14:33 -0500

    update db conn

--- db_conn.js
+++ db_conn.js
@@ -17,12 +17,3 @@ if(!client){
 exports.client = client;
 */
 
-var pg = require('pg');
-
-pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-  client.query('SELECT * FROM your_table', function(err, result) {
-    done();
-    if(err) return console.error(err);
-    console.log(result.rows);
-  });
-});
